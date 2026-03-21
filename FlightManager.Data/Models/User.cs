0f6b6230ff243a6c3b1a96e378d385691ec69f87
MartinNN07 <martinnedev07@gmail.com>
2026-03-21 16:35:48 +0200

    Created file structure and an User model.

---
+++ FlightManager.Data/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FlightManager.Data.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public required string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public required string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "EGN must be exactly 10 digits.")]
+        public required string EGN { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 5)]
+        public required string Address { get; set; }
+    }
+}
