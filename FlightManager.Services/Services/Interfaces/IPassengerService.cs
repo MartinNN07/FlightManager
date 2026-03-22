e8c158147ef5e684440f7419c89cbc6eb1578bff
Светлозар Сталев <propeepesen@abv.bg>
2026-03-22 18:28:05 +0200

    Създаден PassengerService

---
+++ FlightManager.Services/Services/Interfaces/IPassengerService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlightManager.Data.Models;
+
+namespace FlightManager.Services.Services.Interfaces
+{
+    internal interface IPassengerService
+    {
+        /// <summary>
+        /// Asynchronously retrieves all passengers available in the data source.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a collection of all passengers.
+        /// The collection is empty if no passengers are found.</returns>
+        Task<IEnumerable<Passenger>> GetAllPassengersAsync();
+
+        /// <summary>
+        /// Asynchronously retrieves an passenger by its unique identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the passenger to retrieve. Must be an int. </param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the passenger with the specified
+        /// identifier, or null if no matching passenger is found.</returns>
+        Task<Passenger?> GetPassengerByIdAsync(int passengerId);
+
+        /// <summary>
+        /// Asynchronously retrieves an passenger by its EGN (Единен граждански номер).
+        /// </summary>
+        /// <param name="EGN">The EGN (Единен граждански номер) of the passenger to retrieve. Must be a string. </param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the passenger with the specified
+        /// EGN (Единен граждански номер), or null if no matching passenger is found.</returns>
+        Task<Passenger?> GetPassengerByEGNAsync(string passengerEGN);
+
+        /// <summary>
+        /// Asynchronously retrieves an passenger by its reservation unique identifier.
+        /// </summary>
+        /// <param name="reservationId">The reservation unique identifier of the passenger to retrieve. Must be an int. </param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the passenger with the specified
+        /// reservation unique identifier, or null if no matching passenger is found.</returns>
+        Task<Passenger?> GetPassengerByReservationIdAsync(int reservationId);
+
+        /// <summary>
+        /// Asynchronously creates a new passenger record using the specified passenger details.
+        /// </summary>
+        /// <param name="passenger">The passenger entity containing the details to be created. Cannot be null.</param>
+        /// <returns>A task that represents the asynchronous create operation.</returns>
+        Task CreatePassengerAsync(Passenger passenger);
+
+        /// <summary>
+        /// Asynchronously updates the details of the specified passenger in the data store.
+        /// </summary>
+        /// <param name="updatedPassenger">The passenger entity containing updated information. Cannot be null.</param>
+        /// <returns>A task that represents the asynchronous update operation.</returns>
+        Task UpdatePassengerAsync(Passenger updatedPassenger);
+
+        /// <summary>
+        /// Asynchronously deletes the passenger with the specified identifier.
+        /// </summary>
+        /// <param name="passengerId">The unique identifier of the passenger to delete. Cannot be null or empty.</param>
+        /// <returns>A task that represents the asynchronous delete operation.</returns>
+        Task DeletePassengerAsync(int passengerId);
+
+        /// <summary>
+        /// Determines asynchronously whether an passenger with the specified identifier exists.
+        /// </summary>
+        /// <param name="passengerId">The unique identifier of the passenger to check for existence. Cannot be null or empty.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains <see langword="true"/> if the
+        /// passenger exists; otherwise, <see langword="false"/>.</returns>
+        Task<bool> PassengerExistsAsync(int passengerId);
+
+
+    }
+}
---
+++ FlightManager.Services/Services/PassengerService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlightManager.Data;
+using FlightManager.Data.Models;
+using FlightManager.Services.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightManager.Services.Services
+{
+    public class PassengerService : IPassengerService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PassengerService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> PassengerExistsAsync(int passengerId)
+        {
+            return await _context.Passengers.AnyAsync(p => p.Id == passengerId);
+        }
+        public async Task<IEnumerable<Passenger>> GetAllPassengersAsync()
+        {
+            return await _context.Passengers.ToListAsync();
+        }
+        public async Task<Passenger?> GetPassengerByIdAsync(int passengerId)
+        {
+            return await _context.Passengers.FindAsync(passengerId);
+        }
+        public async Task<Passenger?> GetPassengerByEGNAsync(string passengerEGN)
+        {
+            return await _context.Passengers.Where(p => p.EGN == passengerEGN).FirstOrDefaultAsync();
+        }
+        public async Task<Passenger?> GetPassengerByReservationIdAsync(int reservationId)
+        {
+            return await _context.Passengers.FindAsync(reservationId);
+        }
+        public async Task CreatePassengerAsync(Passenger passenger)
+        {
+            await _context.Passengers.AddAsync(passenger);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdatePassengerAsync(Passenger updatedPassenger)
+        {
+            Passenger? Passenger = await _context.Passengers.FindAsync(updatedPassenger.Id);
+
+            if (Passenger is null)
+            {
+                throw new KeyNotFoundException($"Passsenger with ID '{updatedPassenger.Id}' not found.");
+            }
+
+            Passenger.FirstName = updatedPassenger.FirstName;
+            Passenger.MiddleName = updatedPassenger.MiddleName;
+            Passenger.LastName = updatedPassenger.LastName;
+            Passenger.EGN = updatedPassenger.EGN;
+            Passenger.PhoneNumber = updatedPassenger.PhoneNumber;
+            Passenger.Nationality = updatedPassenger.Nationality;
+            Passenger.ReservationId = updatedPassenger.ReservationId;
+            _context.Passengers.Update(Passenger);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeletePassengerAsync(int passengerId)
+        {
+            Passenger? Passenger = await _context.Passengers.FindAsync(passengerId);
+
+            if (Passenger is null)
+            {
+                throw new KeyNotFoundException($"Passenger with ID '{passengerId}' not found.");
+            }
+
+            _context.Passengers.Remove(Passenger);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
