3cff856d1ed3239d661298aa02f7934b90e73278
MartinNN07 <martinnedev07@gmail.com>
2026-03-31 19:53:53 +0300

    Created Airport ViewModels.

---
+++ FlightManager/ViewModels/Airports/AirportCreateViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightManager.Web.ViewModels.Airports
+{
+    public class AirportCreateViewModel
+    {
+        [Required(ErrorMessage = "IATA кодът е задължителен.")]
+        [RegularExpression(@"^[A-Z]{3}$", ErrorMessage = "IATA кодът трябва да бъде точно 3 главни букви.")]
+        [Display(Name = "IATA код")]
+        public string IataCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Държавата е задължителна.")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Името на държавата трябва да е между 2 и 30 символа.")]
+        [Display(Name = "Държава")]
+        public string Country { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Градът е задължителен.")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Името на града трябва да е между 2 и 30 символа.")]
+        [Display(Name = "Град")]
+        public string City { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Името на летището е задължително.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Името на летището трябва да е между 2 и 100 символа.")]
+        [Display(Name = "Летище")]
+        public string AirportName { get; set; } = string.Empty;
+    }
+}
---
+++ FlightManager/ViewModels/Airports/AirportIndexViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightManager.Web.ViewModels.Airports
+{
+    public class AirportIndexViewModel
+    {
+        [Display(Name = "IATA код")]
+        public string IataCode { get; set; } = string.Empty;
+
+        [Display(Name = "Държава")]
+        public string Country { get; set; } = string.Empty;
+
+        [Display(Name = "Град")]
+        public string City { get; set; } = string.Empty;
+
+        [Display(Name = "Летище")]
+        public string AirportName { get; set; } = string.Empty;
+
+        [Display(Name = "Заминаващи полети")]
+        public int DepartingFlightsCount { get; set; }
+
+        [Display(Name = "Пристигащи полети")]
+        public int ArrivingFlightsCount { get; set; }
+    }
+}
